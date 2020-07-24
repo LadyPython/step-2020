8dd43fc2c98882e2d4a0e223e054d39389cf64a2
Sofia <sofia.ladypython@gmail.com>
2020-07-24 12:52:18 +0000

    Style changes (quotes)

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -1,23 +1,23 @@
 // Copyright 2019 Google LLC
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the 'License');
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     https://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an 'AS IS' BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 /** Fetches messages from the server and adds them to the DOM. */
 function loadMessages() {
-  var number_messages = document.getElementById("number-messages").value;
+  var number_messages = document.getElementById('number-messages').value;
   fetch('/list-messages?number_messages='+number_messages).then(response => response.json()).then((messages) => {
     const messageListElement = document.getElementById('messages-list');
-    messageListElement.innerHTML = "";
+    messageListElement.innerHTML = '';
     messages.forEach((message) => {
       messageListElement.appendChild(createMessageElement(message));
     })
@@ -26,24 +26,24 @@ function loadMessages() {
 
 /** Validate the form and add warning if something wrong. */
 function processMessageForm() {
-  document.getElementById("message-warn-container").innerText = "";
+  document.getElementById('message-warn-container').innerText = '';
   
-  var text = document.getElementById("text").value;
+  var text = document.getElementById('text').value;
 
   if (text == null || text.trim().length == 0) {
-    document.getElementById("message-warn-container").innerText = "Please write a message.\n";
+    document.getElementById('message-warn-container').innerText = 'Please write a message.';
     return false;
   }
   return true;
 }
 
 function processNicknameForm() {
-  document.getElementById("nickname-warn-container").innerText = "";
+  document.getElementById('nickname-warn-container').innerText = '';
   
-  var nickname = document.getElementById("nickname").value;
+  var nickname = document.getElementById('nickname').value;
 
   if (nickname == null || nickname.trim().length == 0) {
-    document.getElementById("nickname-warn-container").innerText = "Please write a nickname.\n";
+    document.getElementById('nickname-warn-container').innerText = 'Please write a nickname.\n';
     return false;
   }
   return true;
@@ -51,27 +51,27 @@ function processNicknameForm() {
 
 /** Creates an element that represents a message, including its delete button. */
 function createMessageElement(message) {
-  const messageElement = document.createElement("li");
+  const messageElement = document.createElement('li');
 
-  const timeElement = document.createElement("span");
-  timeElement.setAttribute("class", "timestamp");
+  const timeElement = document.createElement('span');
+  timeElement.setAttribute('class', 'timestamp');
   timeElement.innerText = formatTimestamp(message.timestamp);
 
-  const nameElement = document.createElement("span");
-  nameElement.setAttribute("class", "nickname");
+  const nameElement = document.createElement('span');
+  nameElement.setAttribute('class', 'nickname');
   nameElement.innerText = message.nickname;
 
-  const textElement = document.createElement("span");
-  textElement.setAttribute("class", "text");
+  const textElement = document.createElement('span');
+  textElement.setAttribute('class', 'text');
   textElement.innerText = message.text;
   
-  fetch("/get-uid").then(response => response.json()).then((current_uid) => {
+  fetch('/get-uid').then(response => response.json()).then((current_uid) => {
     console.log(current_uid, message.uid)
     if (message.uid == current_uid) {
-      const deleteButtonElement = document.createElement("button");
-      deleteButtonElement.setAttribute("class", "delete");
-      deleteButtonElement.innerHTML="<img src=\"images/trash.svg\">";
-      deleteButtonElement.addEventListener("click", () => {
+      const deleteButtonElement = document.createElement('button');
+      deleteButtonElement.setAttribute('class', 'delete');
+      deleteButtonElement.innerHTML='<img src="images/trash.svg">';
+      deleteButtonElement.addEventListener('click', () => {
         deleteMesage(message);
 
         // Remove the message from the DOM.
@@ -95,32 +95,32 @@ function formatTimestamp(timestamp) {
 /** Tells the server to delete the message. */
 function deleteMesage(message) {
   const params = new URLSearchParams();
-  params.append("id", message.id);
-  fetch("/delete-message", {method: "POST", body: params});
+  params.append('id', message.id);
+  fetch('/delete-message', {method: 'POST', body: params});
   loadMessages();
 }
 
 function loadCV() {
-  const CVContainer = document.getElementById("cv-container");
-  CVContainer.innerText = "Opening...";
+  const CVContainer = document.getElementById('cv-container');
+  CVContainer.innerText = 'Opening...';
 }
 
 function loadNicknameForm() {
-  fetch("/is-logged-in").then(response => response.json()).then((isLoggedIn) => {
+  fetch('/is-logged-in').then(response => response.json()).then((isLoggedIn) => {
     if (!isLoggedIn) {
-      document.getElementById("nickname-container").hidden = true;
-      fetch("/get-login-url").then(response => response.json()).then((URL) => {
-        document.getElementById("log-container").innerHTML = "<p>Want to choose a nickname? Please, <a href=" + URL + ">login</a></p>";
+      document.getElementById('nickname-container').hidden = true;
+      fetch('/get-login-url').then(response => response.json()).then((URL) => {
+        document.getElementById('log-container').innerHTML = `<p>Want to choose a nickname? Please, <a href=${URL}>login</a></p>`;
       });
     } else {
-      document.getElementById("nickname-container").hidden = false;
-      fetch("/get-logout-url").then(response => response.json()).then((URL) => {
-        document.getElementById("log-container").innerHTML = "<p>You can <a href=" + URL + ">logout</a></p>";
+      document.getElementById('nickname-container').hidden = false;
+      fetch('/get-logout-url').then(response => response.json()).then((URL) => {
+        document.getElementById('log-container').innerHTML = `<p>You can <a href=${URL}>logout</a></p>`;
       });
       
-      fetch("/get-nickname").then(response => response.json()).then((nickname) => {
-        if (nickname != "" && nickname != null) {
-          document.getElementById("nickname").value = nickname;
+      fetch('/get-nickname').then(response => response.json()).then((nickname) => {
+        if (nickname != '' && nickname != null) {
+          document.getElementById('nickname').value = nickname;
         }
       });
     }
@@ -128,23 +128,23 @@ function loadNicknameForm() {
 }
 
 function loadMessageForm() {
-  fetch("/is-logged-in").then(response => response.json()).then((isLoggedIn) => {
+  fetch('/is-logged-in').then(response => response.json()).then((isLoggedIn) => {
     if (!isLoggedIn) {
-      document.getElementById("message-container").hidden = true;
-      fetch("/get-login-url").then(response => response.json()).then((URL) => {
-        document.getElementById("log-container").innerHTML = "<p>Want to send a message? Please, <a href=" + URL + ">login</a></p>";
+      document.getElementById('message-container').hidden = true;
+      fetch('/get-login-url').then(response => response.json()).then((URL) => {
+        document.getElementById('log-container').innerHTML = `<p>Want to send a message? Please, <a href=${URL}>login</a></p>`;
       });
     } else {
-      document.getElementById("message-container").hidden = false;
-      fetch("/get-logout-url").then(response => response.json()).then((URL) => {
-        document.getElementById("log-container").innerHTML = "<p>You can <a href=" + URL + ">logout</a></p>";
+      document.getElementById('message-container').hidden = false;
+      fetch('/get-logout-url').then(response => response.json()).then((URL) => {
+        document.getElementById('log-container').innerHTML = `<p>You can <a href=${URL}>logout</a></p>`;
       });
       
-      fetch("/get-nickname").then(response => response.json()).then((nickname) => {
-        if (nickname != "" && nickname != null) {
-          document.getElementById("nickname-container").innerHTML = "<p><span class=\"nickname\">" + nickname + "</span> Change <a href=\"/nickname.html\">here</a>.</p>";
+      fetch('/get-nickname').then(response => response.json()).then((nickname) => {
+        if (nickname != '' && nickname != null) {
+          document.getElementById('nickname-container').innerHTML = `<p><span class=\'nickname\'>${nickname}</span>Change <a href='/nickname.html'>here</a>.</p>`;
         } else {
-          document.getElementById("nickname-container").innerHTML = "<p>You don't have a nickname. Choose <a href=\"/nickname.html\">here</a>.</p>";
+          document.getElementById('nickname-container').innerHTML = '<p>You don\'t have a nickname. Choose <a href="/nickname.html">here</a>.</p>';
         }
       });
     }
@@ -156,8 +156,8 @@ function loadMessageForm() {
  */
 
 function getData() {
-  fetch("/data").then(response => response.json()).then((count) => {
-    document.getElementById("data-container").innerText = count;
+  fetch('/data').then(response => response.json()).then((count) => {
+    document.getElementById('data-container').innerText = count;
   });
 }
 
@@ -165,7 +165,7 @@ function getData() {
  * Adds a random placeholder.
  */
 function getRandomPlaceholder(elementName) {
-  const placeholders = ["I love cheesecake jelly-o sweet.", "I love gingerbread bear claw marshmallow toffee cookie donut marzipan jelly-o.", "Cupcake jujubes donut croissant I love."];
+  const placeholders = ['I love cheesecake jelly-o sweet.', 'I love gingerbread bear claw marshmallow toffee cookie donut marzipan jelly-o.', 'Cupcake jujubes donut croissant I love.'];
 
   // Pick a random placeholder.
   const placeholder = placeholders[Math.floor(Math.random() * placeholders.length)];
@@ -178,11 +178,11 @@ function getRandomPlaceholder(elementName) {
  * Adds a random greeting to the page.
  */
 function addRandomGreeting() {
-  const greetings = ["Hello world!", "¡Hola Mundo!", "你好，世界！", "Bonjour le monde!", "Привет мир!", "Բարեւ աշխարհ!"];
+  const greetings = ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!', 'Привет мир!', 'Բարեւ աշխարհ!'];
   // Pick a random greeting.
   const greeting = greetings[Math.floor(Math.random() * greetings.length)];
 
   // Add it to the page.
-  const greetingContainer = document.getElementById("greeting-container");
+  const greetingContainer = document.getElementById('greeting-container');
   greetingContainer.innerText = greeting;
 }
